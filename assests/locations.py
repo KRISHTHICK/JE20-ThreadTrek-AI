4e5d66411f981b8873b3f1ac79653efbb4294023
KRISHTHICK <79474339+KRISHTHICK@users.noreply.github.com>
2025-06-20 23:17:43 +0530

    Create locations.py

---
+++ assests/locations.py
@@ -0,0 +1,7 @@
+{
+  "japan": "Avoid flashy outfits in temples. Wear slip-on shoes for easy temple access.",
+  "uae": "Dress conservatively in public. Shoulders and knees should be covered.",
+  "bali": "Wear a sarong and sash when visiting temples.",
+  "new york": "Trendy layers are in! Mix comfort with style for walking around.",
+  "switzerland": "Warm layers and waterproofs are essential. Practical yet chic."
+}
